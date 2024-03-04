6030f7675d036a0c304a1a377d8bafccf958758e
ErrorPower2001 <ErrorPower2001@outlook.com>
2024-03-05 01:01:50 +0800

    New scripts.
    feat: adb.Shizuku.start.sh
    	通过 adb 启动 Shizuku 服务，sh 端
    feat: linux.clash_profile.download_and_config.sh
    	下载并配置 clash 的配置文件

---
+++ adb.Shizuku.start.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/sh
+
+#adb shell sh /storage/emulated/0/Android/data/moe.shizuku.privileged.api/start.sh;
+#
+adb tcpip 5555 && \
+adb kill-server && \
+adb \
+    shell sh '/storage/emulated/0/Android/data/moe.shizuku.privileged.api/start.sh'
---
+++ linux.clash_profile.download_and_config.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/sh
+
+wget \
+    --output-document=$HOME/.config/clash/config.yaml \
+    $1
