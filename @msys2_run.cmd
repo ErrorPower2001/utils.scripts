8580abb2e3a63fcfdf5b2ca80e8c3982b7c90d9d
ErrorPower2001 <ErrorPower2001@outlook.com>
2024-09-28 21:55:36 +0800

    fix: 修改 msys2_run 中 msys2 的默认路径

--- @msys2_run.cmd
+++ @msys2_run.cmd
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-SET "MSYS2=C:\msys2;%UserProfile%\scoop\apps\msys2\current\usr\bin;%UserProfile%\scoop\apps\msys2-cn\current\usr\bin"
+SET "MSYS2=C:\msys64;%UserProfile%\scoop\apps\msys2\current\usr\bin;%UserProfile%\scoop\apps\msys2-cn\current\usr\bin"
 SET "PATH=%MSYS2%;%PATH%"
 %~n0.exe %*
 
